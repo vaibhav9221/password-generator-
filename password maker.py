ca15f47090be013203c42fdd32665afb8038b1c9
vaibhav9221 <59912875+vaibhav9221@users.noreply.github.com>
2020-10-27 11:19:03 -0700

    Update password maker.py

--- password maker.py
+++ password maker.py
@@ -1,53 +1,27 @@
-# import string
-# import random
-#
-# if __name__ == '__main__':
-#
-#     # Letter Sets
-#     s1 = string.ascii_letters
-#     s2 = string.digits
-#     s3 = string.punctuation
-#     service_name = input("Enter the site: ")
-#     plen = int(input("Enter the password length: "))
-#
-#     # Combining all the sets
-#     s = []
-#     s.extend(list(s1))
-#     s.extend(list(s2))
-#     s.extend(list(s3))
-#     # print(s)
-#     random.shuffle(s)
-#     # print (s)
-#
-#     print("Your password is: ")
-#     print("".join(s[0:plen]))
-#
-#
-#     # Saving the password in the file
-#     file = open("pass.txt" , "a")
-#     file.write(f"\n{service_name} ; ")
-#     file.write("".join(s[0:plen]))
-#     file.close()
-#
-#     print("Password has been saved in the file.")
-#
+
+#import string and random module
 import string
 import random
 if __name__ == '__main__':
+    #using ascii function of string module 
     s1=string.ascii_letters
     s2=string.digits
     s3=string.punctuation
+    #name of the site for which you are creating password
     site=input("enter the name of site")
+    #length of password
     passlen=int(input("Enter the number of length of your password:\n"))
     s=[]
     s.extend(s1)
     s.extend(s2)
     s.extend(s3)
-
+    
+    #suffling the password 
     random.shuffle(s)
     print("".join(s[0:passlen]))
 
-
+    #creating a file
     with open ("passcreator.txt","a") as f:
+        #writing in a file
         f.write(f'\n{site} :'+"".join(s[0:passlen]))
         f.close()
